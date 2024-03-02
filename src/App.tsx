58520d5177db9a4980051d2f22048aae0cfbdc4c
mac <mac@MacBook-Pro-3.local>
2024-03-02 19:58:31 +0200

    routes v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-
-import Home from "./pages/Home";
-import SigninForm from './pages/_auth/forms/SigninForm';
-import SignupForm from './pages/_auth/forms/SignupForm';
 
 function App() {
   return (
     <>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/sign-in" element={<SigninForm />} />
-            <Route path="/sign-up" element={<SignupForm />} />
-          </Route>
-        </Routes>
+      <h1>App</h1>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home/> ,
+  },
+  {
+    path: "/Profile",
+    element:<Profile/>,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
